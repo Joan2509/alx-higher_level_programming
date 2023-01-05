7a871e7674ed3824acabc994e066317532ef03db
Joan2509 <wjoanmumbi@gmail.com>
2023-01-05 11:34:46 -0800

    more classes all

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+S
